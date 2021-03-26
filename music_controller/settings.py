7af703fb5ddb2f98e0bff87c0787de50608de8a5
Michel Atieno <mishqamish@gmail.com>
2021-03-26 15:05:27 +0300

    Almost gave up, player is back up playing current song, fixed error in database. Added postgresql as the database. Updated tables in models.

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'music_controller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'music_controller',
+        'USER': 'michel',
+    'PASSWORD': 'mishy',
     }
 }
 
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TIME_ZONE = 'Africa/Nairobi'
